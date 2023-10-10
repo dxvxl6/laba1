5439b33a4d6694262e88a85ead1372b3d43b8efc
dxvxl6 <147540545+dxvxl6@users.noreply.github.com>
2023-10-10 23:36:58 +0300

    Add files via upload

---
+++ laba1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите коэффициент а");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент c");
+double c = Convert.ToDouble(Console.ReadLine());
+//Формула абсциссы
+double x =-b*2*a;
+//Формула ординаты
+double y = a * (x * x) + b * x + c;
+Console.WriteLine($"Абсцисса (x) ={x}");
+Console.WriteLine($"Ордината (y) ={y}");
